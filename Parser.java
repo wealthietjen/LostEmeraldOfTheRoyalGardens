2df0dddbe1204e319f623ff44344430a76a176ce
Wealthie Tjendera <wealthietjendera09@gmail.com>
2023-12-04 05:32:32 +0000

    parser documentation done

--- Parser.java
+++ Parser.java
@@ -44,7 +44,7 @@ public class Parser
 
         inputLine = reader.nextLine();
 
-        // Find up to two words on the line.
+        // Find up to three words on the line.
         Scanner tokenizer = new Scanner(inputLine);
         if(tokenizer.hasNext()) {
             word1 = tokenizer.next();      // get first word
